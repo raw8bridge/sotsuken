23f087a2117b694c5ae25cd74b1f1578586ef2b8
raw8bridge <achappy09tk@gmail.com>
2021-01-22 14:53:42 +0900

    commit

--- test_session.php
+++ test_session.php
@@ -6,4 +6,6 @@ var_dump($_SESSION);
 echo("</pre>");
 ?>
 
-<a href="./test/clrsec.php">SESSION削除</a>
+<a href="./test/clrsec.php">SESSION[test]削除</a>
+<br>
+<a href="./test/Logout.php">ログアウト(SESSION完全削除)</a>
