98e84acd16cfd520421cdcbd8eeb39fa6a54241e
raw8bridge <achappy09tk@gmail.com>
2021-06-19 18:28:25 +0900

    commit

--- test/test_finish.php
+++ test/test_finish.php
@@ -10,12 +10,12 @@ $test_name = $_SESSION['ans'][0]['test_name'];
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>回答完了ページ</title>
+    <title>解答完了ページ</title>
 </head>
 
 <body>
-<h2>回答完了</h2>
-<p><?php echo(hsc($test_name)); ?>の回答を送信しました。</p>
+<h2>解答完了</h2>
+<p><?php echo(hsc($test_name)); ?>の解答を送信しました。</p>
 <a href="Main_student.php">メイン画面に戻る</a>
 </body>
 
